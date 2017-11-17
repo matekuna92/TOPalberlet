e61270d21a6361eba1e6a6cb576aae9f63e51b1f
matekuna92 <kunamate66@gmail.com>
2017-11-17 22:21:09 +0100

    Users posts displaying, but need better solution

--- app/Http/Controllers/MyPostsController.php
+++ app/Http/Controllers/MyPostsController.php
@@ -3,6 +3,7 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use App\Http\Requests;
 use App\User;
 use App\Post;
 use App\Photo;
--- app/User.php
+++ app/User.php
@@ -38,12 +38,6 @@ class User extends Authenticatable
         return $this->hasMany('App\Post');
     }
 
-    public function favouriteProducts() /* Many to many kapcsolat pivot táblával */
-    {
-        return $this->morphedByMany(Post::class, 'favouriteable')
-            ->withPivot(['created_at']) /* Kiszedjük a mező értékét is a pivot táblából, mely időpontban lett hozzáadva a kedvencekhez -opcionális- */
-            ->orderBy('pivot_created_at', 'desc'); /* Legutóbb hozzáadott elem jelenik meg elsőként */
-    }
 
     /* Példa mutatorra, azonban nem használjuk */
 
